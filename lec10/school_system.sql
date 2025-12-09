49a4e8725cd89ec74542e8c39ecbba89df0c5904
mohanedSalaheldin <mohnsd1162002@gmail.com>
2025-12-09 14:48:43 +0200

    school_system database

---
+++ lec10/school_system.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE school_system;
+
+USE school_system;
+
+
+CREATE TABLE Students (
+	id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    birthdate date,
+    address varchar(255),
+    parent_phone_num varchar(12)
+);
+
+
+CREATE TABLE Instructors (
+	id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    department varchar(255),
+    salary double,
+    address varchar(255),
+    phone_num varchar(12)
+);
+
+CREATE TABLE Courses (
+	id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    description varchar(255),
+    instructor_id int,
+    FOREIGN KEY (instructor_id) REFERENCES Instructors(id)
+);
+
+CREATE TABLE Enrollments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT,
+    course_id INT,
+    FOREIGN KEY (student_id) REFERENCES Students(id),
+    FOREIGN KEY (course_id) REFERENCES Courses(id)
+);
+
+INSERT INTO Students (name, birthdate, address, parent_phone_num) VALUES
+('Ali Hassan', '2002-06-11', 'Cairo', '01213168412'),
+('Sara Ahmed', '2003-04-18', 'Giza', '01099887411'),
+('Mohaned Salah', '2001-10-23', 'Alexandria', '01588741236'),
+('Omar Fathy', '2004-01-05', 'Mansoura', '01147896521'),
+('Reem Adel', '2002-12-12', 'Zagazig', '01277889456');
+
+INSERT INTO Instructors (name, department, salary, address, phone_num) VALUES
+('Dr. Mohamed Ali', 'Computer Science', 15000, 'Cairo', '01011223344'),
+('Dr. Salma Ibrahim', 'Math', 14000, 'Giza', '01122334455'),
+('Dr. Hany Saad', 'Physics', 13000, 'Alexandria', '01233445566');
+
+INSERT INTO Courses (name, description, instructor_id) VALUES
+('Intro to Programming', 'Learn programming basics', 1),
+('Calculus I', 'Math course for beginners', 2),
+('Physics I', 'Basic physics principles', 3),
+('Data Structures', 'Core CS data structures', 1);
+
+INSERT INTO Enrollments (student_id, course_id) VALUES
+(1, 1),
+(1, 4),
+(2, 2),
+(3, 1),
+(3, 3),
+(4, 4),
+(5, 2),
+(5, 3);
+
+
+
+
